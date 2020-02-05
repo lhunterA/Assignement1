e828a942ca4b2c8238aab94e866a5b9dab1f9797
Lindsay Hunter <200425671@student.georgianc.on.ca>
2020-02-04 22:43:48 -0500

    Got code completely functional. Using a method to streamline the code. Issues with output for change. Look at later.

--- 200425671A1/Form1.cs
+++ 200425671A1/Form1.cs
@@ -12,47 +12,63 @@ namespace _200425671A1
 {
     public partial class Form1: Form //main class
     {
+        double change = 0;
+        int counter = 0;
+
         public Form1() //Constructor
         {
+
             InitializeComponent();
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+           
             String total = txtTotal.Text;
             String paid = txtPaid.Text;
-            decimal change;
-           // decimal remainder;
-            //decimal track; //keep track of the change
-            //int output;
-
-            decimal numberT;
-            decimal numberP;
-            if (Decimal.TryParse(total, out numberT) && Decimal.TryParse(paid, out numberP))//if the two inputs are a valid number.
+            double numberT;
+            double numberP;
+            if (Double.TryParse(total, out numberT) && Double.TryParse(paid, out numberP))//if the two inputs are a valid number.
             {
                 change = numberP - numberT; //change is paid - total
                 txtChange.Text = change.ToString(); //output the change to the textbox as a string
 
                 if (numberP >= numberT) //if the paid amount is greater than total. 
                 {
-                    MessageBox.Show("The if's work!");
+                    FindChange(2);
+                    txtToonies.Text = counter.ToString();
+                    counter = 0;
+                    FindChange(1);
+                    txtLoonies.Text = counter.ToString();
+                    counter = 0;
+                    FindChange(0.25);
+                    txtQuarters.Text = counter.ToString();
+                    counter = 0;
+                    FindChange(0.10);
+                    txtDimes.Text = counter.ToString();
+                    counter = 0;
+                    if (change <= 0.02)
+                    {
+                        txtNickels.Text = "0";
+                    }
+                    else
+                    {
+                        txtNickels.Text = "1";
+                    }
+
                 }
                 else
                 {
-                    MessageBox.Show("Please, input a higher paid amount");//
-                    return;
+                    MessageBox.Show("Please, input a higher paid amount");//when paid isn't high enough
+                    return; //return to fix the mistake
                 }
             }
             else
             {
-                MessageBox.Show("Invalid Numeric Input. Please add a valid number.");
-                return;
-            }
-                
-            
-          
-            
-        }
+                MessageBox.Show("Invalid Numeric Input. Please add a valid number.");//when they add a letter
+                return; //let them fix the mistake
+            } 
+        } //end the button calculate click event
 
         private void btnClear_Click(object sender, EventArgs e)
         {
@@ -65,6 +81,16 @@ namespace _200425671A1
             txtQuarters.Text = "";
             txtDimes.Text = "";
             txtNickels.Text = "";
+        }
+
+        public void FindChange(double coin)
+        {
+            while (change>=coin)
+            {
+                change -= coin;
+                counter++;
+            }
+
         }
     }
 }
